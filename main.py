01e5efa54a02f527f541064e77bbebc18751ed7f
ZHan <hanzhijun0117@gmail.com>
2018-09-05 23:09:25 -0400

    twelveth commit

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 
     labels = tf.reshape(correct_label, (-1, num_classes))
 
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels))
 
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     
